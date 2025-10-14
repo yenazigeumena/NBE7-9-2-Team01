0986bbf99896e7af2aaa716894f68814ee616ffd
전현수 <wjsgustn201@gmail.com>
2025-10-14 12:17:14 +0900

    feat: Backend 초기 커밋

---
+++ backend/src/main/java/org/example/povi/PoviApplication.java
@@ -0,0 +1,13 @@
+package org.example.povi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoviApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/example/povi/PoviApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.povi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
